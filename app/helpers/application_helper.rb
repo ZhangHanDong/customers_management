813bc6955584ff5144355c5121222879a237f1fb
Alex <blackanger.z@gmail.com>
2009-01-16 17:58:15 +0800

    add login and modify css

--- app/helpers/application_helper.rb
+++ app/helpers/application_helper.rb
@@ -1,3 +1,9 @@
 # Methods added to this helper will be available to all templates in the application.
 module ApplicationHelper
+  def menu_link_to(text, url_options = {}, html_options = {})
+    if current_page?(url_options)
+      html_options[:class] = 'active'
+    end
+    link_to(text, url_options, html_options)
+  end
 end
--- db/schema.rb
+++ db/schema.rb
@@ -9,13 +9,13 @@
 #
 # It's strongly recommended to check this file into your version control system.
 
-ActiveRecord::Schema.define(:version => 20090114083352) do
+ActiveRecord::Schema.define(:version => 20090115200345) do
 
   create_table "customers", :force => true do |t|
     t.string   "name"
     t.string   "name_py"
     t.string   "notes"
-    t.integer  "quotes",              :limit => 10, :precision => 10, :scale => 0
+    t.decimal  "quotes",              :precision => 10, :scale => 2
     t.string   "email"
     t.string   "fixed_line"
     t.string   "msn"
@@ -44,5 +44,19 @@ ActiveRecord::Schema.define(:version => 20090114083352) do
     t.datetime "created_at"
     t.datetime "updated_at"
   end
+
+  create_table "users", :force => true do |t|
+    t.string   "login",                     :limit => 40
+    t.string   "name",                      :limit => 100, :default => ""
+    t.string   "email",                     :limit => 100
+    t.string   "crypted_password",          :limit => 40
+    t.string   "salt",                      :limit => 40
+    t.datetime "created_at"
+    t.datetime "updated_at"
+    t.string   "remember_token",            :limit => 40
+    t.datetime "remember_token_expires_at"
+  end
+
+  add_index "users", ["login"], :name => "index_users_on_login", :unique => true
 
 end
