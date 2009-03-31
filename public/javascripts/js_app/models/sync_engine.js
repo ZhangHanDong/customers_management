5d1924f1049d5f53dd045da2b74d453459f88fab
Alex <blackanger.z@gmail.com>
2009-03-31 11:16:33 +0800

    modify

--- public/javascripts/js_app/models/sync_engine.js
+++ public/javascripts/js_app/models/sync_engine.js
@@ -1,5 +1,5 @@
 /**
- * ContactData Model
+ * SyncEngine Model
  *
  * Alex(blackanger.z@gmail.com)
  * 2009.3
@@ -7,12 +7,26 @@
 $j.m({ SyncEngine: {
 	mySync: function(){
 		workerPool = google.gears.factory.create('beta.workerpool');
+				
 		workerPool.onmessage = function(a, b, message) {
-			console.log('Received message from worker ' + message.sender + ': \n' + message.body);
+			if (message.sender == monitorchildWorkerId) {
+					if(message.text == 'online'){
+						$('#is-connected').show();
+						$('#is-disconnected').hide();
+					}else if(message.text == 'offline'){
+						$('#is-disconnected').show();
+						$('#is-connected').hide();
+					}
+			}
 		};
-
-		var childWorkerId = workerPool.createWorkerFromUrl('/javascripts/js_app/models/worker.js');
-		workerPool.sendMessage(["3..2..", 1, {helloWorld: "Hello world!"}], childWorkerId);
+        
+        var monitorchildWorkerId = workerPool.createWorkerFromUrl('/javascripts/js_app/models/isonline.js');
+        this.monitorChildWorkerPool(monitorchildWorkerId);
+	},
+	
+	
+	monitorChildWorkerPool: function(monitorchildWorkerId){
+		workerPool.sendMessage(window.location + '?monitor', monitorchildWorkerId);
 	}
 }
 });
